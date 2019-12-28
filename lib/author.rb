aa275b8450300254419b030a886b778d45973f0d
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-12-28 05:10:49 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -13,6 +13,18 @@ class Author
         post.author = self
     end
 
+    def posts
+    Post.all.select {|post| post.author == self}
+end
+def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+end
+def self.post_count
+    Post.all.count
+end
+end
+
 # class Author
 #   attr_accessor :name
 #
