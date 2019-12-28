bd26f93d32c10d6fe8dc6e77f2aa4bd99f269b39
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-12-28 05:12:58 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,31 +1,31 @@
-class Post
-  attr_accessor :title
-
-  @@all = []
-
-  def initialize(title)
-    @title = title
-    @@all << self
-  end
-
-
-  def self.all
-    @@all
-  end
-
-  def author=(author)
-    @author
-  end
-
-  # def author
-    # @
-  # end
-
-  # def author_name
-  #   if self.author
-  #     self.author.name
-  #   else
-  #     nil
-  #   end
-  # end
-end
+# class Post
+#   attr_accessor :title
+#
+#   @@all = []
+#
+#   def initialize(title)
+#     @title = title
+#     @@all << self
+#   end
+#
+#
+#   def self.all
+#     @@all
+#   end
+#
+#   def author=(author)
+#     @author
+#   end
+#
+#   # def author
+#     # @
+#   # end
+#
+#   # def author_name
+#   #   if self.author
+#   #     self.author.name
+#   #   else
+#   #     nil
+#   #   end
+#   # end
+# end
